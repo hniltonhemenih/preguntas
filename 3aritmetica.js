f7892d5efc62c30a626700513ed7445231d0c7fd
hniltonhemenih <hniltonhemenih@gmail.com>
2026-05-02 23:04:07 -0500

    Update 3aritmetica.js

--- 3aritmetica.js
+++ 3aritmetica.js
@@ -1,29 +1,36 @@
 // Inicializamos el mazo si no existe, luego añadimos las preguntas
 window.misPreguntas = window.misPreguntas || [];
 window.misPreguntas.push(
-    { q: "Aritmética - PREG 1:", a: "C", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 2:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 3:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 4:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 5:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 6:", a: "C", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 7:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 8:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 9:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 10:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 11:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 12:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 13:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 14:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 15:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 16:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 17:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 18:", a: "E", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 19:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 20:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 21:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 22:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 23:", a: "E", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 24:", a: "C", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Aritmética - PREG 25:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" }
+// Tema 1
+    { q: "Aritmética - PREG 1:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/arC8wVb.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 2
+    { q: "Aritmética - PREG 1:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/qTrGcUk.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 3
+    { q: "Aritmética - PREG 1:", a: "C", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/zIfG4S6.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 4
+    { q: "Aritmética - PREG 1:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/MDo0Rnt.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 5
+    { q: "Aritmética - PREG 1:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/lBLSwPg.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 6
+    { q: "Aritmética - PREG 1:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/iVcjsBE.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 7
+    { q: "Aritmética - PREG 1:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/ELTkOZM.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 8
+    { q: "Aritmética - PREG 1:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/ac7X4qL.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 9
+    { q: "Aritmética - PREG 1:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/JOrRM0K.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 10
+    { q: "Aritmética - PREG 1:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/GcfCK25.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 11
+    { q: "Aritmética - PREG 1:", a: "C", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/xLJ1Nvn.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 12
+    { q: "Aritmética - PREG 1:", a: "E", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/ToAw5Ne.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 13
+    { q: "Aritmética - PREG 1:", a: "E", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/JCWHmi0.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 14
+    { q: "Aritmética - PREG 1:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/T4rjEvl.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 15
+    { q: "Aritmética - PREG 1:", a: "C", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/iGq65ef.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 16
+    { q: "Aritmética - PREG 1:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "http://imgfz.com/i/KFbm7sP.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" }
 );
