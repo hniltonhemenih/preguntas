1ed6113211303c0ceaf906e5e71b3b33d16534dc
hniltonhemenih <hniltonhemenih@gmail.com>
2026-05-07 16:15:27 -0500

    Update 10anatomia.js

--- 10anatomia.js
+++ 10anatomia.js
@@ -1,29 +1,24 @@
 // Inicializamos el mazo si no existe, luego añadimos las preguntas
 window.misPreguntas = window.misPreguntas || [];
 window.misPreguntas.push(
-    { q: "Química - PREG 1:", a: "C", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 2:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 3:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 4:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 5:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 6:", a: "C", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 7:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 8:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 9:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 10:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 11:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 12:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 13:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 14:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 15:", a: "B", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 16:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 17:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 18:", a: "E", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 19:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 20:", a: "D", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 21:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 22:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 23:", a: "E", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 24:", a: "C", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
-    { q: "Química - PREG 25:", a: "A", inc: ["A", "B", "C", "D", "E"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" }
+    { q: "1. ¿Qué estructura no es estudiada por la anatomía macroscópica?:", a: "C", inc: ["A) corazón", "B) pulmones", "C) linfa", "D) hipófisis", "E) glándulas suprarrenales"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "2. ¿Cuál de las siguientes alternativas no corresponden al nivel químico?:", a: "E", inc: ["A) proteínas", "B) carbohidratos", "C) lípidos", "D) vitaminas", "E) fotones"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "3. Con respecto a la posición anatómica, ¿cuál de las siguientes alternativas no corresponde?:", a: "D", inc: ["A) Cuerpo erguido", "B) Mirada hacia adelante", "C) Miembros superiores extendidos", "D) Palma de las manos hacia abajo", "E) Punta de los pies hacia adelante"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "4. ¿Cuál de los siguientes huesos está en posición proximal al húmero?:", a: "C", inc: ["A) Huesos del carpo", "B) Metacarpianos", "C) Clavícula", "D) Falange proximal", "E) Falange distal"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "5. ¿Qué plano divide al cerebro en hemisferios derecho e izquierdo?:", a: "C", inc: ["A) Plano transversal", "B) Plano coronal", "C) Plano sagital", "D) Plano parasagital", "E) Plano horizontal"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "6. ¿Cuál de los siguientes sistemas regulan principalmente la homeostasis?:", a: "C", inc: ["A) Digestivo y urinario", "B) Endocrino urinario", "C) Nervioso y endocrino", "D) Respiratorio y urinario", "E) Digestivo y respiratorio"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "7. ¿Cuál de los siguientes electrolitos se encuentra en menor concentración en el líquido extracelular en comparación al intracelular?:", a: "C", inc: ["A) $Na^+$", "B) $Cl^-$", "C) $K^+$", "D) $Ca^{++}$", "E) $HCO_3^-$"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "8. El agua representa aproximadamente el 60% del peso corporal ¿En cual de los siguientes espacios se encuentra en mayor proporción?:", a: "E", inc: ["A) Espacio extracelular", "B) Espacio tisular", "C) Espacio intravascular", "D) Espacio intersticial", "E) Espacio intracelular"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "9. La homeostasia puede ser alterada por pérdida de líquidos que supera los $2500\text{ ml/día}$ lo cual conlleva a deshidratación ¿Cuál de las siguientes alternativas, no es considerada como causa de deshidratación?:", a: "D", inc: ["A) Hemorragia profusa", "B) Quemadura", "C) Diarreas", "D) Fractura", "E) Diabetes insípida"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    
+    { q: "Química - PREG 10:", a: "C", inc: ["A) $10\text{ litros}$", "B) $12\text{ litros}$", "C) $14\text{ litros}$", "D) $16\text{ litros}$", "E) $18\text{ litros}$"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" }
+
 );
