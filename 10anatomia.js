36d51c63a2e513bd400ab4b58c0434cf447da959
hniltonhemenih <hniltonhemenih@gmail.com>
2026-05-07 17:15:19 -0500

    Update 10anatomia.js

--- 10anatomia.js
+++ 10anatomia.js
@@ -13,12 +13,12 @@ window.misPreguntas.push(
 
     { q: "6. ¿Cuál de los siguientes sistemas regulan principalmente la homeostasis?:", a: "C", inc: ["A) Digestivo y urinario", "B) Endocrino urinario", "C) Nervioso y endocrino", "D) Respiratorio y urinario", "E) Digestivo y respiratorio"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
 
-    { q: "7. ¿Cuál de los siguientes electrolitos se encuentra en menor concentración en el líquido extracelular en comparación al intracelular?:", a: "C", inc: ["A) $Na^+$", "B) $Cl^-$", "C) $K^+$", "D) $Ca^{++}$", "E) $HCO_3^-$"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "7. ¿Cuál de los siguientes electrolitos se encuentra en menor concentración en el líquido extracelular en comparación al intracelular?:", a: "C", inc: ["A) Na^+", "B) Cl^-", "C) K^+", "D) Ca^{++}", "E) HCO_3^-"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
 
     { q: "8. El agua representa aproximadamente el 60% del peso corporal ¿En cual de los siguientes espacios se encuentra en mayor proporción?:", a: "E", inc: ["A) Espacio extracelular", "B) Espacio tisular", "C) Espacio intravascular", "D) Espacio intersticial", "E) Espacio intracelular"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
 
-    { q: "9. La homeostasia puede ser alterada por pérdida de líquidos que supera los $2500\text{ ml/día}$ lo cual conlleva a deshidratación ¿Cuál de las siguientes alternativas, no es considerada como causa de deshidratación?:", a: "D", inc: ["A) Hemorragia profusa", "B) Quemadura", "C) Diarreas", "D) Fractura", "E) Diabetes insípida"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "9. La homeostasia puede ser alterada por pérdida de líquidos que supera los 2500 ml/día lo cual conlleva a deshidratación ¿Cuál de las siguientes alternativas, no es considerada como causa de deshidratación?:", a: "D", inc: ["A) Hemorragia profusa", "B) Quemadura", "C) Diarreas", "D) Fractura", "E) Diabetes insípida"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
     
-    { q: "Química - PREG 10:", a: "C", inc: ["A) $10\text{ litros}$", "B) $12\text{ litros}$", "C) $14\text{ litros}$", "D) $16\text{ litros}$", "E) $18\text{ litros}$"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" }
+    { q: "Química - PREG 10:", a: "C", inc: ["A) 10 litros", "B) 12 litros", "C) 14 litros", "D) 16 litros", "E) 18 litros"], img: "", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" }
 
 );
