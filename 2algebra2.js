fb37239972a9925a50d09b362a5df1ea30bddcae
hniltonhemenih <hniltonhemenih@gmail.com>
2026-05-08 18:47:11 -0500

    Update and rename 2algebra1.js to 2algebra2.js

---
+++ 2algebra2.js
@@ -0,0 +1,26 @@
+window.misPreguntas = window.misPreguntas || [];
+window.misPreguntas.push(
+    { q: "Álgebra", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/algebra-expo/main/alg-expo1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "Álgebra", a: "A", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/algebra-expo/main/alg-expo2.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "Álgebra", a: "C", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/algebra-expo/main/alg-expo3.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "Álgebra", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/algebra-expo/main/alg-expo4.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "Álgebra", a: "E", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/algebra-expo/main/alg-expo5.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "Álgebra", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/algebra-expo/main/alg-expo6.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "Álgebra", a: "A", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/algebra-expo/main/alg-expo7.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "Álgebra", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/algebra-expo/main/alg-expo8.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "Álgebra", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/algebra-expo/main/alg-expo9.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "Álgebra", a: "E", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/algebra-expo/main/alg-expo10.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "Álgebra", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/algebra-expo/main/alg-expo11.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+
+    { q: "Álgebra", a: "C", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/algebra-expo/main/alg-expo12.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" }
+);
