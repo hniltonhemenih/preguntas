d461c5a60fdf5686598d67db128082977c184b1b
hniltonhemenih <hniltonhemenih@gmail.com>
2026-05-11 15:46:04 -0500

    Update and rename 3aritmetica.js to 3aritmetica1.js

---
+++ 3aritmetica1.js
@@ -0,0 +1,36 @@
+// Inicializamos el mazo si no existe, luego añadimos las preguntas
+window.misPreguntas = window.misPreguntas || [];
+window.misPreguntas.push(
+// Tema 1
+    { q: "Aritmética", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari1%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 2
+    { q: "Aritmética", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari2%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 3
+    { q: "Aritmética", a: "C", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari3%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 4
+    { q: "Aritmética", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari4%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 5
+    { q: "Aritmética", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari5%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 6
+    { q: "Aritmética", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari6%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 7
+    { q: "Aritmética", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari7%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 8
+    { q: "Aritmética", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari8%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 9
+    { q: "Aritmética", a: "A", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari9%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 10
+    { q: "Aritmética", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari10%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 11
+    { q: "Aritmética", a: "C", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari11%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 12
+    { q: "Aritmética", a: "E", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari12%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 13
+    { q: "Aritmética", a: "E", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari13%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 14
+    { q: "Aritmética", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari14%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 15
+    { q: "Aritmética", a: "C", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari15%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+// Tema 16
+    { q: "Aritmética", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgaritmetica/main/ari16%20p1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" }
+);
