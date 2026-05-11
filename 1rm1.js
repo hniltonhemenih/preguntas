c447431dd83c742dec042d49ff23e1b8a0660461
hniltonhemenih <hniltonhemenih@gmail.com>
2026-05-11 15:45:08 -0500

    Update and rename 1rm.js to 1rm1.js

---
+++ 1rm1.js
@@ -0,0 +1,29 @@
+// Inicializamos el mazo si no existe, luego añadimos las preguntas
+window.misPreguntas = window.misPreguntas || [];
+window.misPreguntas.push(
+    { q: "Raz. Matemático", a: "E", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm1.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm2.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm3.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "A", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm4.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "C", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm5.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm6.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm7.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm8.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm9.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "A", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm10.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm11.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "E", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm12.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm13.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "E", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm14.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm15.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm16.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm17.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "C", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm18.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm19.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "C", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm20.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "C", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm21.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "E", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm22.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "B", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm23.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "E", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm24.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" },
+    { q: "Raz. Matemático", a: "D", inc: ["A", "B", "C", "D", "E"], img: "https://raw.githubusercontent.com/hniltonhemenih/imgrm/main/rm25.png", devText: "Desarrollo no disponible.", devImg: "", youtubeId: "" }
+);
